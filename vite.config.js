b10279a1d3c4407ff2b823531b557ee8ea82144e
RoadtoFire <fire.jawwad@gmail.com>
2025-12-13 17:15:39 +0500

    corrected image rendering for chapters

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "React-Derma-Quiz"
-})
+  plugins: [
+    react(),
+    tailwindcss(), // ✅ REQUIRED for Tailwind v4
+  ],
+  base: "/React-Derma-Quiz/",
+});
