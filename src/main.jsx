136f82eca396e44e614258d71343a9f12022df82
RoadtoFire <fire.jawwad@gmail.com>
2026-03-10 02:22:16 +0500

    Adding deployment dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./index.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import './index.css'
+// Change BrowserRouter to HashRouter
+import { HashRouter } from 'react-router-dom' 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename="/React-Derma-Quiz">
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter> 
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </HashRouter>
+  </React.StrictMode>,
 )
